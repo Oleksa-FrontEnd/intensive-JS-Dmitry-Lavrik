6604f17435e27a27b2df42a393a6d05aa4d44226
-=EL=- <IT0671581471@gmail.com>
2021-04-09 06:28:44 +0300

    debug js slider

--- lesson2(OOP)/Script_OOP.js
+++ lesson2(OOP)/Script_OOP.js
@@ -3,37 +3,37 @@ window.onload = function () {
     let btn_next = document.querySelector('.gallery .buttons .next');
 */
     let images = document.querySelectorAll('.gallery .photos img');
-    console.log(images);
+
     let slider = new Slider(images);
 
     document.querySelector('.gallery .buttons .prev').onclick = function () {
-        slider.prev()
+        slider.prev();
     }
     document.querySelector('.gallery .buttons .next').onclick = function () {
-        slider.next()
+        slider.next();
     }
 }
     function Slider(images) {
         this.images = images;
-        let i = 0;
+        this.i = 0;
 
         this.prev = function () {
-            this.images[i].classList.remove('showed', ['showed']);
-            i--;
+            this.images[this.i].classList.remove('showed');
+            this.i--;
 
-            if (i < 0) {
-                i = this.images.length - 1;
+            if (this.i < 0) {
+                this.i = this.images.length - 1;
             }
-            this.images.classList.add('showed', ['showed']);
+            this.images.classList.add('showed');
         }
 
         this.next = function () {
-            this.images[i].classList.remove('showed', ['showed']);
-            i++;
+            this.images[this.i].classList.remove('showed');
+            this.i++;
 
-            if (i > this.images.length) {
-                i = 0;
+            if (this.i > this.images.length) {
+                this.i = 0;
             }
-            this.images.classList.add('showed', ['showed']);
+            this.images.classList.add('showed');
         }
     }
